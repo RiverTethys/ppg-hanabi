ad7872fe255beeed750434d81f920f9dbbcd4bdb
madheime <matthew.heimerdinger@gmail.com>
2014-11-02 19:58:13 -0500

    interpret_clue
    got the resolution of the interpretations worked out (simply, room for
    improvement).  Next: the actual interpretation process.

--- src/HanabiConventionFlows.py
+++ src/HanabiConventionFlows.py
@@ -63,20 +63,46 @@ class ClueConventions(HanabiConventions):
 					event_list.append(HanabiEvent(table.name,p.name,"Clue",None,None,number))
 		return event_list
 				
-	def interpret_clue(self,table,game):			
+	def interpret_clue(self,ev,table,game):			
 		#prepare bools
 		protective = False
 		playing = False
 		stalling = False
 		
 		
+		# for card in self.decks[ev.tgt].deck:
+						# if ev.color:
+							# if (card.color == ev.color or card.color == "H"):
+								# # cards are printed to players in reverse order
+								# if self.depth == 0:
+									# print(" {}".format(len(self.decks[ev.tgt].deck) - self.decks[ev.tgt].deck.index(card)))
+						# elif ev.number:
+							# if (card.number == ev.number):
+								# # cards are printed to players in reverse order
+								# if self.depth == 0:
+									# print(" {}".format(len(self.decks[ev.tgt].deck) - self.decks[ev.tgt].deck.index(card)))
+		
+		#resolve bools
 		if(protective):
-			card_list = [x for x in table.location[table.name]]
-			
+			protected_card = table.discard_q[0]
+			Ibit = Hanabit("conventional","discardability","discardable","neg")
+			table.add_bit(Ibit,protected_card)		
+	
 		if(playing):
-			pass
+			if (ev.color):
+				card_list = [card for card in table.location[table.name] if card.color == ev.color]
+			if (ev.number):
+				card_list = [card for card in table.location[table.name] if card.number == ev.number]
+			playable_card = self.newest(card_list,table)
+			Ibit = Hanabit("conventional","playability","playable","pos")
+			table.add_bit(Ibit,playable_card)
+			if playable_card in table.play_q:
+				table.play_q.remove(playable_card)
+			table.play_q.appendleft(playable_card)
+			
+		
 		if(stalling):
-		#not sure what this is gonna be
+		#not sure what this is gonna be, if anything
 			pass
 				
 				
