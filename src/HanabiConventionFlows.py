1a4310864dc955bccd46403c423f1b08f4a53adc
madheime <matthew.heimerdinger@gmail.com>
2014-11-04 18:04:17 -0500

    The sad part
    in which I realized that all of the possible actions will already be in
    a table somewhere.

--- src/HanabiConventionFlows.py
+++ src/HanabiConventionFlows.py
@@ -31,51 +31,49 @@ class HanabiConventions(object):
 	#def __init__(self,game,active_player):
 		#HanabiConventions.__init__(game)
 	
-	def possible(self,table):
-		event_list = []
-		for card in table.location[table.name]:
-			if table.list[card].query_bit_pile(qcard=card,qquality=["playability"],qspin=["pos"]) or table.list[card].query_bit_pile(qcard=card,qquality=["playability"],qspin=["final"]):
-				event_list.append( HanabiEvent(table.name,None,"Play",card.id,card.color,card.number))
-		return event_list
+	# def possible(self,table):
+		# event_list = []
+		# for card in table.location[table.name]:
+			# if table.list[card].query_bit_pile(qcard=card,qquality=["playability"],qspin=["pos"]) or table.list[card].query_bit_pile(qcard=card,qquality=["playability"],qspin=["final"]):
+				# event_list.append( HanabiEvent(table.name,None,"Play",card.id,card.color,card.number))
+		# return event_list
 
 #class DiscardConventions(HanabiConventions):
 	#def __init__(self,game):
 		#HanabiConventions.__init__(game)
 
-	def possible(self,table):
-		event_list = []
-		for card in table.location[table.name]:
-			if table.list[card].query_bit_pile(qcard=card,qquality=["discardability"],qvalue = ["trash"], qspin=["final"]) or (table.list[card].query_bit_pile(qcard=card,qquality=["discardability"],qvalue=["discardable"],qspin=["pos"]) and not table.list[card].query_bit_pile(qcard=card,qquality=["playability"],qvalue=["playable"],qspin =["pos"])):
-				event_list.append( HanabiEvent(table.name,None,"Discard",card.id,card.color,card.number))
-		return event_list
+	# def possible(self,table):
+		# event_list = []
+		# for card in table.location[table.name]:
+			# if table.list[card].query_bit_pile(qcard=card,qquality=["discardability"],qvalue = ["trash"], qspin=["final"]) or (table.list[card].query_bit_pile(qcard=card,qquality=["discardability"],qvalue=["discardable"],qspin=["pos"]) and not table.list[card].query_bit_pile(qcard=card,qquality=["playability"],qvalue=["playable"],qspin =["pos"])):
+				# event_list.append( HanabiEvent(table.name,None,"Discard",card.id,card.color,card.number))
+		# return event_list
 		
 #class ClueConventions(HanabiConventions):
 #	def __init__(self,game):
 #		HanabiConventions.__init__(game)
 				
-	def possible(self,table):
-		event_list = []
-		for p in self.players:
-			if p.name != table.name:
-				for color in self.bot.decktemplate.colors:
-					event_list.append(HanabiEvent(table.name,p.name,"Clue",None,color,None))
-				for number in self.bot.decktemplate.colors:
-					event_list.append(HanabiEvent(table.name,p.name,"Clue",None,None,number))
-		return event_list
+	# def possible(self,table):
+		# event_list = []
+		# for p in self.players:
+			# if p.name != table.name:
+				# for color in self.bot.decktemplate.colors:
+					# event_list.append(HanabiEvent(table.name,p.name,"Clue",None,color,None))
+				# for number in self.bot.decktemplate.colors:
+					# event_list.append(HanabiEvent(table.name,p.name,"Clue",None,None,number))
+		# return event_list
 	
 	
 	
-	def make_protective_clue(self):
-		pass
+	def predict_clue(self,ev,table):
+		#possible ways the clue could go.
+		protective = False
+		playing = False
+		stalling = False
+		multi_play = False
+		bombing = False
+		
 	
-	def make_playing_clue(self):
-		pass
-	
-	def make_stalling_clue(self):
-		pass
-	
-	def make_multi_play_clue(self):
-		pass
 	
 	def interpret_clue(self,ev,table,game):			
 		#prepare bools
