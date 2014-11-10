ee43093750aac03a260560fb9cdfe2632cd90b55
RiverTethys <river.tethys@gmail.com>
2014-11-09 19:08:23 -0500

    move Event
    Eval needs Events

--- src/HanabiClasses.py
+++ src/HanabiClasses.py
@@ -149,54 +149,6 @@ class HanabiVariant(object):
 		self.stackstemplate = HanabiDeckTemplate(decktemplate.colors,decktemplate.numbers,{x:{y:0 for y in decktemplate.numbers} for x in decktemplate.colors})
 		self.rules = rules
 		
-class HanabiEvent(object):
-	def __init__(self,src,tgt,type,id,color,number):
-		self.src = src
-		self.tgt = tgt
-		self.type = type # "Play", "Discard", or "Clue"
-		self.id = id
-		self.color = color
-		self.number = number
-		self.touch = []
-		
-	def __repr__(self):
-		if (self.src == None):
-			repstr = "Null Hanabi event"
-		else:
-			repstr = "{} decided to {} ".format(self.src,self.type)
-			if (type == "Clue"):
-				repstr += "{}".format(self.tgt)
-				if (self.number != None):
-					repstr += " about {}'s.".format(self.number)
-				elif (self.color != None):
-					repstr += " about {}'s.".format(self.color)
-				else:
-					repstr += "... about what, we may never know."
-			else:
-				repstr += "{}{}.".format(self.number,self.color)
-		return repstr
-	
-	def make_clue(self,src,tgt,color,number):
-		self.src = src
-		self.tgt = tgt
-		self.type = "Clue"
-		self.color = color
-		self.number = number
-
-	def make_play(self,src,id,color,number):
-		self.src = src
-		self.type = "Play"
-		self.id = id
-		self.color = color
-		self.number = number
-
-	def make_discard(self,src,id,color,number):
-		self.src = src
-		self.type = "Discard"
-		self.id = id
-		self.color = color
-		self.number = number
-
 class HanabiGame(object):
 	def __init__(self,name,variant,conventions,deduction_bot,player_name_list,total_depth,depth):
 		self.name = name
--- src/HanabiEvalFlows.py
+++ src/HanabiEvalFlows.py
@@ -2,6 +2,54 @@ from copy import deepcopy
 from HanabiDeductionFlows import *
 from HanabiConventionFlows import *
 
+class HanabiEvent(object):
+	def __init__(self,src,tgt,type,id,color,number):
+		self.src = src
+		self.tgt = tgt
+		self.type = type # "Play", "Discard", or "Clue"
+		self.id = id
+		self.color = color
+		self.number = number
+		self.touch = []
+		
+	def __repr__(self):
+		if (self.src == None):
+			repstr = "Null Hanabi event"
+		else:
+			repstr = "{} decided to {} ".format(self.src,self.type)
+			if (type == "Clue"):
+				repstr += "{}".format(self.tgt)
+				if (self.number != None):
+					repstr += " about {}'s.".format(self.number)
+				elif (self.color != None):
+					repstr += " about {}'s.".format(self.color)
+				else:
+					repstr += "... about what, we may never know."
+			else:
+				repstr += "{}{}.".format(self.number,self.color)
+		return repstr
+	
+	def make_clue(self,src,tgt,color,number):
+		self.src = src
+		self.tgt = tgt
+		self.type = "Clue"
+		self.color = color
+		self.number = number
+
+	def make_play(self,src,id,color,number):
+		self.src = src
+		self.type = "Play"
+		self.id = id
+		self.color = color
+		self.number = number
+
+	def make_discard(self,src,id,color,number):
+		self.src = src
+		self.type = "Discard"
+		self.id = id
+		self.color = color
+		self.number = number
+
 class Choice(object):
 	def __init__(self,action=None,tgt=None,color=None,number=None,pos=None):
 		self.action = action
